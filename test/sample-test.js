b86f637baed4fa192959eceecda37dc8fd59432b
THANAPOLCHIMNAM <thnabig006@gmail.com>
2021-06-22 22:07:19 +0700

    edit test

--- test/sample-test.js
+++ test/sample-test.js
@@ -1,18 +1,31 @@
 const { expect } = require("chai");
 
-describe("Greeter", function() {
+describe("Bank", function() {
   it("Should return the new greeting once it's changed", async function() {
-    const Greeter = await ethers.getContractFactory("Greeter");
-    const greeter = await Greeter.deploy("Hello, world!");
-    await greeter.deployed();
+//--Get wallet account form hardhat
+const [owner, addr1] = await ethers.getSigners();
+// console.log(addr1.address);
 
-    expect(await greeter.greet()).to.equal("Hello, world!");
+//--Call smart contract
+const SimpleBank = await hre.ethers.getContractFactory("SimpleBank");
+const bank = await SimpleBank.deploy();
+await bank.deployed();
 
-    const setGreetingTx = await greeter.setGreeting("Hola, mundo!");
-    
-    // wait until the transaction is mined
-    await setGreetingTx.wait();
+const enroll = await bank.connect(owner).enroll("big");
 
-    expect(await greeter.greet()).to.equal("Hola, mundo!");
+const enroll2 = await bank.connect(addr1).enroll("ohm");
+
+const deposit = await bank.connect(owner).deposit(5000);
+
+const withdraw = await bank.connect(owner).withdraw(1000);
+
+const TransferTo = await bank.connect(owner).TransferTo(addr1.address,1000)
+
+    expect(await bank.wallet(owner.address)[1]).to.equal(3000);
+
+    expect(await bank.wallet(addr1.address)[1]).to.equal(1000);
+
+
+
   });
 });
